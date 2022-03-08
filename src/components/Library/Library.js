ac62d8215e76a3687e5fc403359768b6d4fbe025
Doug Carter <dougc85@gmail.com>
2022-03-07 21:49:06 -0800

    added library component

---
+++ src/components/Library/Library.js
@@ -0,0 +1,12 @@
+import './Library.scss';
+import { React } from 'react';
+
+function Library() {
+  return (
+    <div className="Library">
+      library
+    </div>
+  )
+}
+
+export default Library;
