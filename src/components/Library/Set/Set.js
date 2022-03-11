e65b9a67dd44d39929d6be7215893d0cb664d890
Doug Carter <dougc85@gmail.com>
2022-03-10 19:18:06 -0800

    added color to Set SongEntry listings

--- src/components/Library/Set/Set.js
+++ src/components/Library/Set/Set.js
@@ -2,7 +2,7 @@ import './Set.scss';
 import { React, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import AddSong from '../../AddSong/AddSong';
-import Song from '../Song/Song';
+import SongEntry from '../SongEntry/SongEntry';
 import Loading from '../../Loading/Loading';
 
 function Set(props) {
@@ -21,14 +21,18 @@ function Set(props) {
     loading ?
       <Loading /> :
       <div className="Set">
-        <h2 className="Set-heading">{set.setName}</h2>
-        <button onClick={handleAddButton} className="Set-add">Add a Song</button>
-        {Object.keys(set.allSongs).map((songTitle) => {
-          const { knowledge, createdAt } = set.allSongs[songTitle];
-          return (
-            <Song title={songTitle} knowledge={knowledge} createdAt={createdAt} sortByDateAdded={false} />
-          )
-        })}
+        <div className="Set-header">
+          <h2 className="Set-heading">{set.setName}</h2>
+          <button onClick={handleAddButton} className="Set-add">Add a Song</button>
+        </div>
+        <div className="Set-songs">
+          {Object.keys(set.allSongs).map((songTitle) => {
+            const { knowledge, createdAt } = set.allSongs[songTitle];
+            return (
+              <SongEntry title={songTitle} knowledge={knowledge} createdAt={createdAt} sortByDateAdded={false} key={songTitle} />
+            )
+          })}
+        </div>
         {showAdd && <AddSong set={set} setShowAdd={setShowAdd} user={user} />}
       </div>
 
