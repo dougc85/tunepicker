e3281aefd334994805df2080a8bc4b6d1ceb82f7
Doug Carter <dougc85@gmail.com>
2022-03-14 11:38:14 -0700

    refactored library paths into a component, Path

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -2,6 +2,7 @@ import './Library.scss';
 import { React } from 'react';
 import { Link } from 'react-router-dom';
 import Loading from '../Loading/Loading';
+import Path from './Path/Path';
 
 function Library(props) {
 
@@ -11,12 +12,7 @@ function Library(props) {
     loading ?
       <Loading /> :
       <div className="Library">
-        <div className="Library-path">
-          <svg className="Library-path-icon-library" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M9 3V18H12V3H9M12 5L16 18L19 17L15 4L12 5M5 5V18H8V5H5M3 19V21H21V19H3Z" />
-          </svg>
-          <p className="Library-path-heading">Library</p>
-        </div>
+        <Path heading="Library" pathType="Library" />
         <Link to={'/library/sets'}>Sets</Link>
         <Link to={'/library/allsongs'}>All Songs</Link>
       </div>
