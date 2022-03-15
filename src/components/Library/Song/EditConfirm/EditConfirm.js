c2f96a69fb86e4494fd18570500ee16e63e5184c
Doug Carter <dougc85@gmail.com>
2022-03-14 22:10:17 -0700

    began paragraph/input changing fields in Song component

---
+++ src/components/Library/Song/EditConfirm/EditConfirm.js
@@ -0,0 +1,12 @@
+import './EditConfirm.scss';
+import { React } from 'react';
+
+function EditConfirm() {
+  return (
+    <div className="EditConfirm">
+
+    </div>
+  )
+}
+
+export default EditConfirm;
--- src/components/Library/Song/Song.js
+++ src/components/Library/Song/Song.js
@@ -1,13 +1,15 @@
 import './Song.scss';
-import { React } from 'react';
+import { React, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Path from '../Path/Path';
 import Loading from '../../Loading/Loading';
+import EditConfirm from './EditConfirm/EditConfirm';
 
 function Song(props) {
 
   const { song, loading, getSongData } = props;
   const params = useParams();
+  const [showTitleEdit, setShowTitleEdit] = useState(false);
 
   if (!loading && !song.title) {
     getSongData(params.songTitle);
@@ -22,6 +24,11 @@ function Song(props) {
       <Loading /> :
       <div className="Song">
         <Path heading={capitalizeTitle()} pathType={'Song'} />
+        <div className="Song-fields Song-title">
+          <label htmlFor="songTitle-songPage" className="Song-title-label">Title</label>
+          {showTitleEdit ? <input type="text" className="Song-title-input">{song.title}</input> : <p className="Song-title-value">{song.title}</p>}
+          <EditConfirm />
+        </div>
       </div>
   )
 }
