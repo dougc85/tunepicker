932b138794bcc38a13762251454e056b9d83a4aa
Doug Carter <dougc85@gmail.com>
2022-05-11 12:41:41 -0700

    added ScrollToTop for all route changes

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
 
   const [showAlreadyInLibrary, setShowAlreadyInLibrary] = useState(false);
 
+
   return (
     <div className="App">
       <GlobalStyles />
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { SubContextProvider } from './context/sub-context';
+import ScrollToTop from './hooks/ScrollToTop';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <SubContextProvider>
-        <App />
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </SubContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
