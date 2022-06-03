b21135ff5a4836c771c787e34bdbfbd75e369024
Doug Carter <dougc85@gmail.com>
2022-06-02 18:32:14 -0700

    added functionality to tunesIWantToLearn; also, fixed login bug where Token was not being stored properly in the db

--- src/components/TunesIWantToLearn/TunesIWantToLearn.styled.js
+++ src/components/TunesIWantToLearn/TunesIWantToLearn.styled.js
@@ -21,3 +21,29 @@ export const Header = styled.div`
   }
   
 `
+
+export const TunesStyled = styled.ul`
+  li {
+    padding: 1rem;
+    font-size: 2rem;
+    text-align: start;
+    position: relative;
+
+    &:nth-child(2n-1) {
+      background-color: #fee9e9;
+    }
+
+    &:nth-child(2n) {
+      background-color: #ffd3d3;
+    }
+
+    svg {
+      height: 20px;
+      position: absolute;
+      right: 15px;
+      top: 50%;
+      transform: translateY(-50%);
+      color: rgba(0,0,0, .4);
+    }
+  }
+`;
