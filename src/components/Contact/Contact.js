1fec814c712a0c9bd0dca7e911eebd09d27a5e5a
Doug Carter <dougc85@gmail.com>
2022-06-22 21:43:08 -0700

    added validation to Contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,5 @@
 import { ContactStyled, ContactForm, FormElement, FormElementTextArea, FormElementButton } from './Contact.styled';
-import React from 'react';
+import React, { useState } from 'react';
 import AddButton from '../generics/AddButton.styled';
 import useFormInput from '../../hooks/useFormInput';
 import { send } from 'emailjs-com';
@@ -10,8 +10,40 @@ function Contact() {
   const [subject, handleSubjectChange, resetSubject] = useFormInput('');
   const [message, handleMessageChange, resetMessage] = useFormInput('');
 
+  const [emailError, setEmailError] = useState('');
+  const [subjectError, setSubjectError] = useState('');
+  const [messageError, setMessageError] = useState('');
+
+  const [finishedSend, setFinishedSend] = useState(false);
+
+
   async function handleSubmit(e) {
     e.preventDefault();
+
+    let errors = false;
+
+    if (email.length === 0) {
+      errors = true;
+      setEmailError('*required')
+    } else if (email.indexOf('@') === -1) {
+      errors = true;
+      setEmailError('*must be valid email address')
+    }
+
+    if (subject.length === 0) {
+      errors = true;
+      setSubjectError('*required')
+    }
+
+    if (message.length === 0) {
+      errors = true;
+      setMessageError('*required')
+    }
+
+    if (errors) {
+      return;
+    }
+
 
     const toSend = {
       subject: subject,
@@ -34,28 +66,60 @@ function Contact() {
     resetEmail();
     resetSubject();
     resetMessage();
+    setFinishedSend(true);
+  }
+
+  function emailAndErrors(e) {
+    if (emailError) {
+      setEmailError('');
+    }
+    handleEmailChange(e);
+  }
+
+  function subjectAndErrors(e) {
+    if (subjectError) {
+      setSubjectError('');
+    }
+    handleSubjectChange(e);
+  }
+
+  function messageAndErrors(e) {
+    if (messageError) {
+      setMessageError('');
+    }
+    handleMessageChange(e);
   }
 
   return (
     <ContactStyled>
       <h2>Contact Us</h2>
-      <ContactForm>
+      {!finishedSend && <ContactForm>
         <FormElement>
-          <label>Your Email</label>
-          <input value={email} type="email" required onChange={handleEmailChange} />
+          <label>Your Email
+            <span>{emailError}</span>
+          </label>
+
+          <input value={email} type="email" required onChange={emailAndErrors} />
         </FormElement>
         <FormElement>
-          <label>Subject</label>
-          <input value={subject} type="text" required onChange={handleSubjectChange} autoComplete="off" />
+          <label>Subject
+            <span>{subjectError}</span>
+          </label>
+          <input value={subject} type="text" required onChange={subjectAndErrors} autoComplete="off" />
         </FormElement>
         <FormElementTextArea>
-          <label>Message</label>
-          <textarea value={message} required onChange={handleMessageChange}></textarea>
+          <label>Message
+            <span>{messageError}</span>
+          </label>
+          <textarea value={message} required onChange={messageAndErrors}></textarea>
         </FormElementTextArea>
         <FormElementButton>
           <AddButton onClick={handleSubmit}>Submit</AddButton>
         </FormElementButton>
-      </ContactForm>
+      </ContactForm>}
+      {finishedSend &&
+        <p>Your message has been sent.</p>
+      }
     </ContactStyled>
   )
 }
