16116fdfb14979bca8bfcb793b880f6e75707de5
Doug Carter <dougc85@gmail.com>
2022-07-06 15:52:34 -0700

    added notes to Picker

---
+++ src/components/PickController/Notes/Notes.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Modal from '../../generics/Modal.styled';
+import { NotesStyled } from './Notes.styled';
+
+function Notes(props) {
+
+  const { notes, setShowNotes } = props;
+
+  function handleCancel(e) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    setShowNotes(false);
+  }
+
+  function addWhitespace(str) {
+
+    let elementArray = [];
+    let newString = '';
+
+    for (const char of str) {
+
+      if (char === '\n') {
+        elementArray.push(newString);
+        elementArray.push(<br />);
+        newString = '';
+      } else {
+        newString = newString.concat(char);
+      }
+    }
+    elementArray.push(newString);
+    return elementArray;
+  }
+
+  return (
+    <Modal handleOutsideClick={handleCancel} contentHeight={'30rem'}>
+      <NotesStyled>
+        <h3>Notes</h3>
+        <p>{addWhitespace(notes)}</p>
+      </NotesStyled>
+    </Modal>
+  )
+}
+
+export default Notes;
