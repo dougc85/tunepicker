075ab106839cf888a7b6566cd5b6516634668843
Doug Carter <dougc85@gmail.com>
2022-07-09 18:11:40 -0700

    improved the capitalize.js function to capitalize the letter after a parenthesis, for instance

--- src/helperFunctions/capitalize.js
+++ src/helperFunctions/capitalize.js
@@ -1,3 +1,14 @@
 export default function capitalize(str) {
-  return str.split(' ').map((word) => word[0].toUpperCase().concat(word.substring(1))).join(' ');
+  return str.split(' ').map((word) => {
+    if (word[0].toLowerCase() !== word[0].toUpperCase()) {
+      return word[0].toUpperCase().concat(word.substring(1));
+    }
+
+    if (word.length === 1) {
+      return word;
+    }
+
+    return word[0].concat(capitalize(word.substring(1)))
+  }
+  ).join(' ');
 }
