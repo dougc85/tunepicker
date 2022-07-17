77385a011f25b7944631b5329c240f1d6165d81b
Doug Carter <dougc85@gmail.com>
2022-07-16 17:55:33 -0700

    changed Loading.js to an animation; still needs finessing and ability to be one of 3 or 4 different sizes

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Path from './Path/Path';
 import { LibraryStyled } from './Library.styled';
+import Loading from '../Loading/Loading';
 
 function Library() {
 
   return (
-    <LibraryStyled>
-      <Path heading="Library" pathType="Library" />
-      <Link to={'/library/sets'}>Sets</Link>
-      <Link to={'/library/allsongs'}>All Songs</Link>
-    </LibraryStyled>
+    // <LibraryStyled>
+    //   <Path heading="Library" pathType="Library" />
+    //   <Link to={'/library/sets'}>Sets</Link>
+    //   <Link to={'/library/allsongs'}>All Songs</Link>
+    // </LibraryStyled>
+    <Loading />
   )
 }
 
