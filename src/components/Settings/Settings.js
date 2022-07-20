4c53a96cb11d4b650a6449fd5527f93fd4530118
Doug Carter <dougc85@gmail.com>
2022-07-19 21:41:40 -0700

    began implementing ChangeEmail and ChangePassword

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,21 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import SubContext from '../../context/sub-context';
 import AddButton from '../generics/AddButton.styled';
+import ChangeEmail from './ChangeEmail/ChangeEmail';
 import DeleteAccount from './DeleteAccount/DeleteAccount';
 import { SettingsStyled } from './Settings.styled';
 
 function Settings() {
 
   const [showDeleteAccount, setShowDeleteAccount] = useState(false);
+  const [showChangeEmail, setShowChangeEmail] = useState(false);
+
+  const { user } = useContext(SubContext);
 
   function handleDeleteClick(e) {
     setShowDeleteAccount(true);
   }
 
+  function handleEmailClick(e) {
+    setShowChangeEmail(true);
+  }
+
   return (
     <SettingsStyled>
       <h2>Settings</h2>
+      <div>
+        <div>
+          <label>Email</label>
+          <button onClick={handleEmailClick}>change</button>
+        </div>
+
+        <p>{user.email}</p>
+      </div>
+      <div>
+        <div>
+          <label>Password</label>
+          <button>change</button>
+        </div>
+        <p>&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;</p>
+      </div>
       <AddButton onClick={handleDeleteClick}>Delete Your Account</AddButton>
-      <p>(more settings to come at a later date...)</p>
+      {showChangeEmail && <ChangeEmail setShowChangeEmail={setShowChangeEmail} />}
       {showDeleteAccount && <DeleteAccount setShowDeleteAccount={setShowDeleteAccount} />}
     </SettingsStyled>
   )
