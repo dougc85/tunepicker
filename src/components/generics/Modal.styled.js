278300c90606ef42fefa0809b0cf97aa617c9cc7
Doug Carter <dougc85@gmail.com>
2022-07-25 17:40:05 -0700

    Modal lack of scrolling underneath works even better now

--- src/components/generics/Modal.styled.js
+++ src/components/generics/Modal.styled.js
@@ -37,9 +37,13 @@ function Modal(props) {
 
   useEffect(() => {
     document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+    document.body.style.width = '100vw';
 
     return () => {
       document.body.style.overflow = 'unset';
+      document.body.style.position = 'unset';
+      document.body.style.width = 'unset';
     }
   }, []);
 
